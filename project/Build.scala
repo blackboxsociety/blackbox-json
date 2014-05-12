6be8b86542bdf1f8979ab911a5023b50b9f6c47c
Joseph Moniz <joseph.moniz@gmail.com>
2014-05-11 22:07:06 -0700

    version bump to 0.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object Build extends Build {
     .settings(
       name                  := "blackbox-json",
       organization          := "com.blackboxsociety",
-      version               := "0.1.0",
+      version               := "0.2.0",
       scalaVersion          := "2.11.0",
       licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
       scalacOptions in Test ++= Seq("-Yrangepos"),
